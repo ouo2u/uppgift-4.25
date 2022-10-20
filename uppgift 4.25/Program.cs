ff99b8e0f57bcaf7be4a041701def19752075bc2
ouo2u <abod98451@gmail.com>
2022-10-21 00:35:18 +0200

    Add project files.

---
+++ uppgift 4.25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace uppgift_4._25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv ett hel tal");
+            float heltal = float.Parse(Console.ReadLine());
+            Console.WriteLine("skriv ett annat hel tal");
+            float andraheltal = float.Parse(Console.ReadLine());
+            Console.WriteLine("Välj ett av följande alternativ.");
+            Console.WriteLine("1. Subtrahera ett tal med ett annat");
+            Console.WriteLine("2.Dividera ett tal med ett annat");
+            Console.WriteLine("3.Avsluta programmet");
+            string alterntiv = Console.ReadLine();
+
+            switch (alterntiv)
+            {
+                case "1":
+                    Console.WriteLine("subtriktionen bli" + (heltal - andraheltal));
+                    break;
+                case "2":
+                    Console.WriteLine("divsion blir " + (heltal / andraheltal));
+                    break;
+                case "3":
+                    Console.WriteLine("programmet avslutats");
+                    break;
+            }
+        }
+    }
+}
